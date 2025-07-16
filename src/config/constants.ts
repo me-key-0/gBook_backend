5628969fff6f5502f3b57893673202d02552cc07
me-key-0 <me.key.as.24@gmail.com>
2025-07-16 08:32:13 +0300

    update on constants

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_PRICE = 50; // in ETB
+export const BASE_PRICE = 1// in ETB
 export const DISCOUNT_LIMIT = 20;
 export const DISCOUNT_RATE = 0.25;
-export const EXPECTED_ACCOUNT = '0912345678'; // Telebirr recipient
+export const EXPECTED_ACCOUNT = '0986249172'; // Telebirr recipient
