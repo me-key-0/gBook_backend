606c01e3ae33d918ef50eef6df63949e51a61669
Mikias Wondafrash <121063294+me-key-0@users.noreply.github.com>
2025-07-16 09:05:22 +0300

    Update constants.ts

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_PRICE = 1// in ETB
+export const BASE_PRICE = 4// in ETB
 export const DISCOUNT_LIMIT = 20;
 export const DISCOUNT_RATE = 0.25;
 export const EXPECTED_ACCOUNT = '2519****9172'; // Telebirr recipient
